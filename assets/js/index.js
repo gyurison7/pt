4c3989247bf1b30ff062aeaeba384475b52a5526
gyuri.son <gyuri.son@weaverloft.com>
2024-06-26 14:59:34 +0900

    feat: 작업 완료

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,16 @@ $(".content-box a").hover(
         $(this).closest(".project").find(".left-area").removeClass("on");
     }
 );
+
+gsap.from(".footer .tag span", {
+    scrollTrigger: {
+        trigger: ".footer",
+        start: "0% 70%",
+        end: "100% 0%",
+        toggleActions: "play none none reverse",
+        // markers: true,
+    },
+    stagger: 0.3,
+    yPercent: 100,
+    ease: "power2.inOut",
+});
